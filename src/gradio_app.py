61faa7e7142304f74269b441ad83c3a6b120565e
sky_kondo <kondo@skysystem.co.jp>
2023-10-21 00:23:29 +0900

    Slackボットを実装

--- src/gradio_app.py
+++ src/gradio_app.py
@@ -2,9 +2,10 @@ import random
 import time
 import gradio as gr
 from dotenv import load_dotenv
-from chatbot_engine import chat
+from chatbot_engine import chat, create_index
 from langchain.memory import ChatMessageHistory
 import os
+
 
 def respond(message, chat_history):
   history = ChatMessageHistory()
@@ -13,7 +14,7 @@ def respond(message, chat_history):
     history.add_ai_message(ai_message)
       
   print(chat_history)
-  bot_message = chat(message, history)
+  bot_message = chat(message, history, index)
   chat_history.append((message, bot_message))
   # time.sleep(1)
   return "", chat_history
@@ -39,4 +40,5 @@ if __name__ == "__main__":
   else:
     auth = None
     
+  index = create_index()
   demo.launch(auth = auth)
